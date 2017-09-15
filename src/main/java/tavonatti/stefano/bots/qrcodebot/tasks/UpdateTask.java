3049313468ca1e04a6c9e55fd16f61aaa74ba70b
Stefano Tavonatti <stefanotavonatti93@gmail.com>
2017-09-15 21:28:56 +0200

    encode and send qrcode

--- src/main/java/tavonatti/stefano/bots/qrcodebot/tasks/UpdateTask.java
+++ src/main/java/tavonatti/stefano/bots/qrcodebot/tasks/UpdateTask.java
@@ -2,11 +2,15 @@ package tavonatti.stefano.bots.qrcodebot.tasks;
 
 import com.google.zxing.*;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
 import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import org.apache.log4j.Logger;
 import org.telegram.telegrambots.api.methods.GetFile;
+import org.telegram.telegrambots.api.methods.send.SendDocument;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.api.objects.File;
 import org.telegram.telegrambots.api.objects.PhotoSize;
 import org.telegram.telegrambots.api.objects.Update;
@@ -14,6 +18,7 @@ import org.telegram.telegrambots.exceptions.TelegramApiException;
 import tavonatti.stefano.bots.qrcodebot.QrCodeBot;
 
 import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -27,6 +32,10 @@ import java.util.Map;
 public class UpdateTask implements Runnable {
 
     public static Logger logger=Logger.getLogger(UpdateTask.class);
+
+    private final static String CHARSET="UTF-8"; // or "ISO-8859-1"
+    private final static int WIDTH_QR=500;
+    private final static int HEIGHT_QR=500;
 
     private QrCodeBot qrCodeBot;
     private Update update;
@@ -76,8 +85,45 @@ public class UpdateTask implements Runnable {
                         text+=splits[i];
                     }
 
-                    message.setText(text);
-                    break;
+                    SendPhoto sendPhoto=new SendPhoto();
+                    sendPhoto.setChatId(update.getMessage().getChatId());
+
+                    /*create Qrcode */
+                    BufferedImage bufferedImage;
+                    try {
+                        bufferedImage=createQr(text);
+                    } catch (WriterException e) {
+                        sendErrorMessage("Unable to encode the image");
+                        e.printStackTrace();
+                        return;
+                    } catch (IOException e) {
+                        sendErrorMessage("Unable to encode the image");
+                        e.printStackTrace();
+                        return;
+                    }
+
+                    /*get a InputStream with the qrcode*/
+                    ByteArrayOutputStream baos=new ByteArrayOutputStream();
+
+                    try {
+                        ImageIO.write(bufferedImage,"jpg",baos);
+                        baos.flush();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    InputStream is=new ByteArrayInputStream(baos.toByteArray());
+
+                    /*send the qrcode*/
+                    sendPhoto.setNewPhoto("qrcode",is);
+
+                    try {
+                        qrCodeBot.sendPhoto(sendPhoto);
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+
+                    return;
 
                 default:
                     message.setText("Type /help for the list of available commands");
@@ -138,8 +184,7 @@ public class UpdateTask implements Runnable {
                 return;
             }
 
-            Map hintMap = new HashMap();
-            hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+            Map hintMap = getHintMap();
 
             SendMessage message=new SendMessage();
             message.setChatId(update.getMessage().getChatId());
@@ -160,6 +205,25 @@ public class UpdateTask implements Runnable {
 
         }
 
+    }
+
+    private Map getHintMap() {
+        Map hintMap = new HashMap();
+        hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+        return hintMap;
+    }
+
+    private BufferedImage createQr(String text) throws WriterException, IOException{
+        //String charset, Map hintMap, int qrCodeheight, int qrCodewidth)
+        Map hintMap = getHintMap();
+
+        BitMatrix matrix = new MultiFormatWriter().encode(
+                new String(text.getBytes(CHARSET), CHARSET),
+                BarcodeFormat.QR_CODE, WIDTH_QR, HEIGHT_QR, hintMap);
+
+        BufferedImage bufferedImage=MatrixToImageWriter.toBufferedImage(matrix);
+
+        return bufferedImage;
     }
 
     private InputStream getFileInputStream(File file) throws IOException {
